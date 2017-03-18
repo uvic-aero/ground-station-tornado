a42b04e0d31ed8adc9eeaf5b2051471bca2bae8a
Chris Hampu <chrishampu@outlook.com>
2017-03-17 18:24:48 -0700

    Give images more variety

--- groundstation/simulator.py
+++ groundstation/simulator.py
@@ -1,13 +1,34 @@
 from .websocket import pubsub
 from tornado import ioloop
 import json
+import random
 
-test_image = {
+test_images = [{
     'type': 'image',
     'width': 671,
     'height': 475,
     'url': 'http://stories.barkpost.com/wp-content/uploads/2015/04/husky.jpg'
-}
+}, {
+    'type': 'image',
+    'width': 1280,
+    'height': 854,
+    'url': 'https://www.pets4homes.co.uk/images/breeds/43/large/81f6538eb4101d364ac0588b10040f0e.jpg'
+}, {
+    'type': 'image',
+    'width': 880,
+    'height': 564,
+    'url': 'http://static.boredpanda.com/blog/wp-content/uploads/2015/08/siberian-husky-dog-instagram-erica-tcogoeva-20.jpg'   
+}, {
+    'type': 'image',
+    'width': 960,
+    'height': 648,
+    'url': 'https://pbs.twimg.com/media/CUb38ljVAAENDyE.jpg:large'
+}, {
+    'type': 'image',
+    'width': 620,
+    'height': 443,
+    'url': 'http://i.dailymail.co.uk/i/pix/2014/01/10/article-2537126-1A8B1A2E00000578-874_634x453.jpg'
+}]
 
 test_telemetry = {
     'type': 'telemetry',
@@ -33,7 +54,7 @@ class Simulator:
                 continue
             for sub in subscribers[type]:
                 print("Sending images")
-                sub.write_message(json.dumps(test_image))
+                sub.write_message(json.dumps(test_images[random.randint(0, len(test_images)-1)]))
 
     def send_telemetry(self):
 
