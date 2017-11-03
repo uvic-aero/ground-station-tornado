70f4ecc85609bea6c93a6c9fdbc0a8414607dd7c
Wenli Huang <Davidhuangwenli@gmail.com>
2017-11-02 19:13:30 -0700

    Implement image file system functions

--- groundstation/image.py
+++ groundstation/image.py
@@ -1,12 +1,13 @@
 import uuid
+from PIL import Image as PILImage
+
 
 # Basic concept of an image
 # Can have image data received over the network or loaded from the filesystem,
 # telemetry data that's been matched in, and the idea of a persistent file location
 class Image:
-    def __init__(self, uuid=None):
-
-        self._uuid = uuid.uuid4() if uuid is None else uuid # Unique identifier used by database. If it does not exist, create it
+    def __init__(self, uuid_param=None):
+        self._uuid = uuid.uuid4() if uuid_param is None else uuid_param  # Unique identifier used by database. If it does not exist, create it
         self.jpeg_data = None
         self.file_location = None
         self.timestamp = None
@@ -14,12 +15,16 @@ class Image:
 
     # Use the filesystem location to open this image
     # File location must exist
-    def load_jpeg_from_filesysem(self):
-        pass
+    def load_jpeg_from_filesystem(self):  # typo
+        self.jpeg_data = PILImage.open(self.file_location)
 
     # Save jpeg data in memory to the filesystem & record file location
+    # Save to './images/{self._uuid}.jpg'
     def save_jpeg_to_filesystem(self):
-        pass
+        location = "./images/" + str(self._uuid) + ".jpg"
+        self.jpeg_data.save(location)
+        self.file_location = location
+
 
     # Use telemetry service to find telemetry for this image/timestamp
     # The timestamp in this image must exist and be valid
