cb839d15efe5bb98f7e4dd1661248bbdd0d046e6
Dragon Prevost <dragon@dkp.io>
2018-02-10 09:30:36 -0800

    by_coordinates response update

--- groundstation/routes/by_coordinates.py
+++ groundstation/routes/by_coordinates.py
@@ -1,12 +1,15 @@
 from tornado import web
 import tornado
-import sys
-sys.path.append('../database.py')
+#import sys
+#sys.path.append('../database.py')
 from ..database import database
 from motor import motor_asyncio
 import motor
 import asyncio
 import json
+import pprint
+import array
+from bson.json_util import loads
 
 class ImageByCoordinateHandler(web.RequestHandler):
     @web.asynchronous
@@ -17,6 +20,10 @@ class ImageByCoordinateHandler(web.RequestHandler):
         loop = asyncio.get_event_loop()
         loop.create_task(database.find_by_coordinates(a, b, self._images_received))
     def _images_received(self, images):
-        # send json result
-        self.write(json.dumps(images))
+        #in json format
+        #images = loads(images)
+        #images = bytearray(images)
+        #images = json.loads(json.dumps(images))
+        strr = str(images)
+        self.write(strr)
         self.finish()
