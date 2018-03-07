8431b2c9f2f7880863a5b170d9f4607c370d913b
Dragon Prevost <dragon@dkp.io>
2018-03-07 15:40:46 -0800

    removal of test code

--- groundstation/routes/by_coordinates.py
+++ groundstation/routes/by_coordinates.py
@@ -1,7 +1,5 @@
 from tornado import web
 import tornado
-#import sys
-#sys.path.append('../database.py')
 from ..database import database
 from motor import motor_asyncio
 import motor
@@ -24,10 +22,5 @@ class ImageByCoordinateHandler(web.RequestHandler):
         loop = asyncio.get_event_loop()
         loop.create_task(database.find_by_coordinates(a, b, self._images_received))
     def _images_received(self, images):
-        #in json format
-        #images = loads(images)
-        #images = bytearray(images)
-        #images = json.loads(json.dumps(images))
-        #strr = str(images)
         self.write(images)
         self.finish()
