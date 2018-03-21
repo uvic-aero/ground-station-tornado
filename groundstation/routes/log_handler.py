b7814322d8a54cf631ddef1a23bbbc034eebcb70
Dragon Prevost <dragon@dkp.io>
2018-03-21 15:45:29 -0700

    commiting before pull, shouldnt be any changes

--- groundstation/routes/log_handler.py
+++
@@ -1,9 +0,0 @@
-from tornado import web
-import json
-from ..log_service import log_service
-
-class LogHandler(web.RequestHandler):
-
-    def post(self):
-        log_dict = json.loads(self.request.body, encoding = object)
-        log_service.handle_log(log_dict)
