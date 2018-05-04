30f8a54f59f89d8f460d8b39dc2c8ca010e2ae3d
Unknown <chrishampu@outlook.com>
2018-05-04 09:24:24 -0700

    Set default groundstation url

--- groundstation/constants.py
+++ groundstation/constants.py
@@ -3,4 +3,4 @@
 camera_url = "http://192.168.0.113:8000"
 
 # Change to local API IP address of Ground Station running this application
-groundstation_url = "http://localhost:24002"
+groundstation_url = "http://192.168.0.110:24002"
