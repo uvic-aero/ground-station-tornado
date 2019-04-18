509ffae2f04d684be8433e60b0c7fdbcc25b9068
dragonprevost <dragon@dkp.io>
2019-04-17 19:19:22 -0700

    setting alt to 0

--- groundstation/database.py
+++ groundstation/database.py
@@ -163,8 +163,8 @@ class Database:
         for document in await cursor.to_list(length=None):
             #temp_telemetry = await self._telemetry_collection.find_one({'_id': ObjectId(document['telemetry_id'])})
             telemetry = literal_eval(document['telemetry'])
-            if not telemetry['alt']:
-                telemetry['alt'] = 0
+            
+            telemetry['alt'] = 0
 
             temp.append(
                 {
